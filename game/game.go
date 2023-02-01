a4d2bbed844919ca1ff66fea060c839008f984aa
duexcoast <conor.ux@gmail.com>
2023-02-01 15:55:44 -0500

    interfaces and methods

--- game/game.go
+++ game/game.go
@@ -3,19 +3,52 @@ package main
 import "fmt"
 
 func main() {
-	var i1 Item
-	fmt.Printf("%#v\n", i1)
-
-	i2 := Item{1, 2}
-	fmt.Printf("%#v\n", i2)
-
 	i3 := Item{
 		Y: 10,
 	}
-	fmt.Printf("%#v\n", i3)
+	i1 := Item{
+		X: 45,
+		Y: 10,
+	}
+	i2 := Item{
+		X: 200,
+		Y: 50,
+	}
+	fmt.Printf("i3: %#v\n", i3)
 
-	fmt.Println(NewItem(10, 20))
-	fmt.Println(NewItem(10, -20))
+	(&i3).Move(5, 50)
+	fmt.Printf("i3 (move): %#v\n", i3)
+
+	p1 := Player{
+		Name: "Cornelius",
+		Item: Item{500, 500},
+	}
+	fmt.Printf("p1: %#v\n", p1)
+
+	p1.Move(20, 20)
+	fmt.Printf("p1 (move): %#v\n", p1)
+
+	fmt.Printf("p1.Item.X;%#v\n", p1.Item.X)
+
+	ms := []mover{
+		i1,
+		p1,
+		i2,
+	}
+	moveAll(ms, 0, 0)
+	for _, m := range ms {
+		fmt.Println(m)
+	}
+}
+
+func moveAll(ms []mover, x, y int) {
+	for _, m := range ms {
+		m.Move(x, y)
+	}
+}
+
+type mover interface {
+	Move(x, y int)
 }
 
 func NewItem(x, y int) (*Item, error) {
@@ -29,6 +62,16 @@ func NewItem(x, y int) (*Item, error) {
 	return &i, nil
 }
 
+type Player struct {
+	Name string
+	Item // embed Item
+	T
+}
+
+type T struct {
+	X int
+}
+
 const (
 	maxX = 1000
 	maxY = 600
@@ -38,4 +81,11 @@ const (
 type Item struct {
 	X int
 	Y int
+}
+
+// i is called "the receiver"
+// if you want to mutate, use pointer receiver
+func (i *Item) Move(x, y int) {
+	i.X = x
+	i.Y = y
 }

ba9ff2353b07b120d8bea8cc957fee662dd7bc3d
duexcoast <conor.ux@gmail.com>
2023-02-01 12:38:36 -0500

    sha1 program that unzips gzip file and returns sha1 checksum

--- sha1/sha1.go
+++ sha1/sha1.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,20 +16,42 @@ func main() {
 		log.Fatalf("error: %s", err)
 	}
 	fmt.Println(sig)
+	sig, err = sha1Sum("sha1.go")
+	if err != nil {
+		log.Fatalf("error: %s", err)
+	}
+	fmt.Println(sig)
+
 }
 
-// $ cat http.log.gz | gunzip | sha1sum
+/*
+if file names end with .gz
+
+	$ cat http.log.gz | gunzip | sha1sum
+
+else
+
+	$ cat http.log.gz | sha1sum
+*/
 func sha1Sum(fileName string) (string, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return "", err
 	}
 	defer file.Close()
+	var r io.Reader = file // we create a new variable of type io.Reader, and assign file to it.
+	// now we can assign gz (type *gzip.Reader) to the same var as file. previously their types
+	// conflicted. but the io.Reader interface allows us to find common ground.
 
-	r, err := gzip.NewReader(file)
-	if err != nil {
-		return "", err
+	if strings.HasSuffix(fileName, ".gz") {
+		gz, err := gzip.NewReader(file)
+		if err != nil {
+			return "", err
+		}
+		defer gz.Close()
+		r = gz
 	}
+
 	w := sha1.New()
 	// io.CopyN(os.Stdout, r, 100)
 

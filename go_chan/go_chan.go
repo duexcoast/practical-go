c9e691fee35cb885ab0e350b00fb87fcb89a0c5c
duexcoast <conor.ux@gmail.com>
2023-02-02 14:25:31 -0500

    goroutines and channels

--- go_chan/go_chan.go
+++ go_chan/go_chan.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	go fmt.Println("goroutine")
-	fmt.Println("main")
+	ch := make(chan Person)
+	// go func() {
+	// 	ch <- "hi" // send
+	// }()
+	// msg := <-ch // receive
+	// fmt.Println(msg)
 
-	time.Sleep(10 * time.Millisecond)
+	go func() {
+		for i := 0; i < 3; i++ {
+			// msg := fmt.Sprintf("message #%d", i+1)
+			person := Person{
+				Age: i + 1,
+			}
+			ch <- person
+		}
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Println("got:", msg)
+	}
+	msg := <-ch
+	fmt.Println(msg)
+
+	msg, ok := <-ch // ch is closed
+	fmt.Printf("closed: %#v (ok=%v)", msg, ok)
+
+}
+
+type Person struct {
+	Age int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("Age: %d", p.Age)
 }
